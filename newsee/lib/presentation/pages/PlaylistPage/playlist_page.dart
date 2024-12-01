6a25a7dcc87d602c02ee90a6d8f525af848895eb
Hyojung <147718654+HyojunGenius@users.noreply.github.com>
2024-12-02 02:27:42 +0900

    [fix] Playlist 디자인 수정

--- newsee/lib/presentation/pages/PlaylistPage/playlist_page.dart
+++ newsee/lib/presentation/pages/PlaylistPage/playlist_page.dart
@@ -151,53 +151,120 @@ class PlaylistPageState extends State<PlaylistPage> {
                     ? playlists[index]
                     : subscribePlaylists[index];
                 return GestureDetector(
-                  onTap: () =>
-                      _navigateToPlaylistDetail(playlist), // 클릭 시 상세 페이지로 이동
+                  onTap: () => _navigateToPlaylistDetail(playlist),
                   child: Container(
-                    margin: const EdgeInsets.only(
-                        top: 10, left: 24, right: 24, bottom: 10),
-                    padding: const EdgeInsets.only(
-                        top: 12, left: 17, right: 17, bottom: 12),
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 10),
+                    padding: const EdgeInsets.all(16),
                     decoration: BoxDecoration(
                       color: Colors.white,
-                      borderRadius: BorderRadius.circular(16),
+                      borderRadius: BorderRadius.circular(10),
                       boxShadow: [
                         BoxShadow(
                           color: Colors.black.withOpacity(0.1),
-                          spreadRadius: 0,
-                          blurRadius: 8,
-                          offset: const Offset(0, 4),
+                          blurRadius: 5,
+                          offset: const Offset(0, 2),
                         ),
                       ],
                     ),
-                    child: SizedBox(
-                      height: 113, // 원하는 고정 높이를 설정
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            playlist.playlistName,
-                            style: const TextStyle(fontSize: 20),
-                          ),
-                          Text(
-                            playlist.description.length > 43
-                                ? '${playlist.description.substring(0, 43)}...'
-                                : playlist.description,
-                            style: const TextStyle(
-                                fontSize: 12, color: Colors.grey),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                "게시자: ${playlist.userId}",
-                                style: const TextStyle(fontSize: 14),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // 제목 및 카운트 + 버튼
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Expanded(
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    child: Text(
+                                      playlist.playlistName,
+                                      style: const TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black,
+                                      ),
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                  const SizedBox(width: 8), // 제목과 카운트 간의 간격
+                                  Text(
+                                    "(${playlist.news.length})",
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ],
                               ),
-                            ],
+                            ),
+                            IconButton(
+                              icon: const Icon(
+                                Icons.more_vert,
+                                size: 20, // 버튼 크기
+                              ),
+                              onPressed: () {
+                                // 점 세 개 버튼 클릭 시 실행
+                                showModalBottomSheet(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return Wrap(
+                                      children: [
+                                        ListTile(
+                                          leading: const Icon(Icons.edit),
+                                          title: const Text('수정'),
+                                          onTap: () {
+                                            Navigator.pop(context);
+                                            // 수정 동작 추가
+                                          },
+                                        ),
+                                        ListTile(
+                                          leading: const Icon(Icons.delete),
+                                          title: const Text('삭제'),
+                                          onTap: () {
+                                            Navigator.pop(context);
+                                            // 삭제 동작 추가
+                                          },
+                                        ),
+                                      ],
+                                    );
+                                  },
+                                );
+                              },
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 8),
+
+                        // 설명
+                        Text(
+                          playlist.description.length > 60
+                              ? '${playlist.description.substring(0, 60)}...'
+                              : playlist.description,
+                          style: const TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey,
                           ),
-                        ],
-                      ),
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        const SizedBox(height: 12),
+
+                        // 게시자
+                        Row(
+                          children: [
+                            Text(
+                              "게시자: ${playlist.userId}",
+                              style: const TextStyle(
+                                fontSize: 10,
+                                color: Colors.black54,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
                   ),
                 );
