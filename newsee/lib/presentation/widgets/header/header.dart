d6ce7eae948fe318243126b1609ee32f3198d171
Hyojung <147718654+HyojunGenius@users.noreply.github.com>
2024-12-02 01:41:32 +0900

    [fix] header 디자인 수정

--- newsee/lib/presentation/widgets/header/header.dart
+++ newsee/lib/presentation/widgets/header/header.dart
@@ -1,46 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:newsee/presentation/pages/SearchPage/SearchPage.dart'; // SetAlert import 추가
+import 'package:newsee/presentation/pages/SearchPage/SearchPage.dart';
 
 class Header extends StatelessWidget implements PreferredSizeWidget {
-  final int visibilityFlag; // 파라미터 추가
+  final int visibilityFlag;
 
   const Header({
     Key? key,
-    this.visibilityFlag = 1, // 기본값 1로 설정, -1일 경우 GestureDetector 안 보이게
+    this.visibilityFlag = 1,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
-    double appBarHeight = kToolbarHeight; // AppBar의 기본 높이를 사용
+    double appBarHeight = kToolbarHeight;
 
     return AppBar(
-      toolbarHeight: appBarHeight, // AppBar 높이 설정
-      elevation: 0, // 그림자 없애기
-      backgroundColor: Colors.transparent, // 배경 투명하게 설정
-      titleSpacing: 0, // 기본 AppBar titleSpacing 없애기
-      automaticallyImplyLeading: false, // 기본 왼쪽 아이콘 제거
+      toolbarHeight: appBarHeight,
+      elevation: 0,
+      backgroundColor: Colors.transparent,
+      titleSpacing: 0,
+      automaticallyImplyLeading: false,
       title: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: screenWidth * 0.05, // 좌우 패딩을 화면 크기 비율로 설정
-        ),
+        padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween, // 좌우로 요소 배치
-          crossAxisAlignment: CrossAxisAlignment.center, // 세로 중앙 정렬
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            // 왼쪽 로고 아이콘
             Flexible(
               child: Image.asset(
                 'assets/logo.png',
-                height: appBarHeight * 0.5, // 고정된 높이로 크기 조정
-                fit: BoxFit.contain, // 이미지를 컨테이너에 맞춤
+                height: appBarHeight * 0.5,
+                fit: BoxFit.contain,
               ),
             ),
-            // 오른쪽 메뉴 아이콘 (visibilityFlag 조건 추가)
-            if (visibilityFlag != -1) // visibilityFlag가 -1이 아닐 때만 표시
+            if (visibilityFlag != -1)
               GestureDetector(
                 onTap: () {
-                  // search 아이콘 클릭 시 SearchPage로 이동
                   Navigator.push(
                     context,
                     MaterialPageRoute(builder: (context) => SearchPage()),
@@ -49,16 +44,22 @@ class Header extends StatelessWidget implements PreferredSizeWidget {
                 child: const Icon(
                   Icons.search,
                   color: Color(0xFF0038FF),
-                  size: kToolbarHeight * 0.5, // 고정된 크기 설정
+                  size: kToolbarHeight * 0.5,
                 ),
               ),
           ],
+        ),
+      ),
+      bottom: PreferredSize(
+        preferredSize: const Size.fromHeight(1.0),
+        child: Container(
+          color: const Color(0xFFD4D4D4),
+          height: 1.0,
         ),
       ),
     );
   }
 
   @override
-  Size get preferredSize =>
-      const Size.fromHeight(kToolbarHeight); // AppBar 높이 설정
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
