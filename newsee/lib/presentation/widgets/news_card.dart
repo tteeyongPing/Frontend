f496914fcb6284b3468605decf2a542a4dffe6d5
Hyojung <147718654+HyojunGenius@users.noreply.github.com>
2024-12-05 21:43:46 +0900

    [Design] News Card 디자인 수정

--- newsee/lib/presentation/widgets/news_card.dart
+++ newsee/lib/presentation/widgets/news_card.dart
@@ -26,75 +26,77 @@ class NewsCard extends StatelessWidget {
         );
       },
       child: Container(
-        height: 140,
-        margin: const EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 20),
-        padding: const EdgeInsets.all(16),
+        margin: const EdgeInsets.only(top: 10, left: 24, right: 24, bottom: 10),
+        padding:
+            const EdgeInsets.only(top: 12, left: 18, right: 18, bottom: 12),
         decoration: BoxDecoration(
           color: Colors.white,
-          borderRadius: BorderRadius.circular(12),
+          borderRadius: BorderRadius.circular(16),
           boxShadow: [
             BoxShadow(
               color: Colors.black.withOpacity(0.1),
-              blurRadius: 6,
+              spreadRadius: 0,
+              blurRadius: 8,
               offset: const Offset(0, 4),
             ),
           ],
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  news.company,
-                  style: const TextStyle(
-                      fontSize: 12,
-                      fontWeight: FontWeight.normal,
-                      color: Colors.black54),
-                ),
-                if (isEditing)
-                  GestureDetector(
-                    onTap: () {
-                      onSelected(!news.selected);
-                    },
-                    child: Icon(
-                      news.selected
-                          ? Icons.check_circle
-                          : Icons.radio_button_unchecked,
-                      color:
-                          news.selected ? const Color(0xFF4D71F6) : Colors.grey,
-                    ),
-                  )
-                else
+        child: SizedBox(
+          height: 120,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
                   Text(
-                    news.categoryId,
+                    news.company,
                     style: const TextStyle(
                         fontSize: 12,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFF0038FF)),
+                        fontWeight: FontWeight.normal,
+                        color: Colors.black54),
                   ),
-              ],
-            ),
-            const SizedBox(height: 8),
-            Text(
-              news.title,
-              style: const TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                  height: 1.5),
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
-            Text(
-              news.content,
-              style: const TextStyle(
-                  fontSize: 16, color: Colors.grey, height: 1.5),
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-            ),
-          ],
+                  if (isEditing)
+                    GestureDetector(
+                      onTap: () {
+                        onSelected(!news.selected);
+                      },
+                      child: Icon(
+                        news.selected
+                            ? Icons.check_circle
+                            : Icons.radio_button_unchecked,
+                        color: news.selected
+                            ? const Color(0xFF4D71F6)
+                            : Colors.grey,
+                      ),
+                    )
+                  else
+                    Text(
+                      news.categoryId,
+                      style: const TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF0038FF)),
+                    ),
+                ],
+              ),
+              // const SizedBox(height: 8),
+              Text(
+                news.title,
+                style: const TextStyle(fontSize: 20, color: Colors.black),
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Text(
+                news.content,
+                style: const TextStyle(
+                    fontSize: 12, color: Colors.grey, height: 1.5),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ],
+          ),
         ),
       ),
     );
